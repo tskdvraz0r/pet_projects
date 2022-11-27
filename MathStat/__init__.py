ae5885d313d21de7a346dfbcbb769d97ee24290d
tskdvraz0r <ts.kdv.raz0r@ya.ru>
2022-11-27 16:17:06 +0300

    added new Progression defs

--- MathStat/__init__.py
+++ MathStat/__init__.py
@@ -56,6 +56,28 @@ class Math:
 
                 return a_1 + diff * (a_n - 1)
 
+            @staticmethod
+            def num_list(a_1: float, diff: float, qnt: float) -> list:
+                """
+                An arithmetic progression or arithmetic sequence (AP) is a sequence of numbers such that the difference
+                between the consecutive terms is constant. For instance, the sequence 5, 7, 9, 11, 13, 15, . . . is an
+                arithmetic progression with a common difference of 2.
+                (c) wiki
+
+                :param a_1: the first member of the progression
+                :param diff: progression difference
+                :param qnt: quantity of progression members
+                :return: list(f(x) = a_1 + diff)
+                """
+                prog_lst: list = [a_1]
+
+                while qnt > 1:
+                    prog_lst.append(a_1 + diff)
+                    a_1 = prog_lst[-1]
+                    qnt -= 1
+
+                return prog_lst
+
         class Geometric:
             @staticmethod
             def n_num(b_1: float, diff: float, b_n: float) -> float:
@@ -75,6 +97,29 @@ class Math:
 
                 return b_1 * diff ** (b_n - 1)
 
+            @staticmethod
+            def num_list(a_1: float, diff: float, qnt: float) -> list:
+                """
+                In mathematics, a geometric progression, also known as a geometric sequence, is a sequence of non-zero
+                numbers where each term after the first is found by multiplying the previous one by a fixed,
+                non-zero number called the common ratio. For example, the sequence 2, 6, 18, 54, ... is a geometric
+                progression with common ratio 3. Similarly 10, 5, 2.5, 1.25, ... is a geometric sequence with common
+                ratio 1/2.
+                (c) wiki
+
+                :param a_1: the first member of the progression
+                :param diff: progression difference
+                :param qnt: quantity of progression members
+                :return: list(f(x) = a_1 * diff)
+                """
+                prog_lst: list = [a_1]
+
+                while qnt > 1:
+                    prog_lst.append(a_1 * diff)
+                    a_1 = prog_lst[-1]
+                    qnt -= 1
+
+                return prog_lst
 
 class Statistic:
     pass
