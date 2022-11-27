1452c21fb83ff5ca359e642b560db64bf0903e1c
tskdvraz0r <ts.kdv.raz0r@ya.ru>
2022-11-27 16:02:28 +0300

    fix progression defs

--- MathStat/__init__.py
+++ MathStat/__init__.py
@@ -39,39 +39,41 @@ class Math:
         return (a + b) ** 2
 
     class Progression:
-        @staticmethod
-        def arithmetic(a_1: float, diff: float, a_n: float) -> float:
-            """
-            An arithmetic progression or arithmetic sequence (AP) is a sequence of numbers such that the difference
-            between the consecutive terms is constant. For instance, the sequence 5, 7, 9, 11, 13, 15, . . . is an
-            arithmetic progression with a common difference of 2.
-            (c) wiki
+        class Arithmetic:
+            @staticmethod
+            def n_num(a_1: float, diff: float, a_n: float) -> float:
+                """
+                An arithmetic progression or arithmetic sequence (AP) is a sequence of numbers such that the difference
+                between the consecutive terms is constant. For instance, the sequence 5, 7, 9, 11, 13, 15, . . . is an
+                arithmetic progression with a common difference of 2.
+                (c) wiki
 
-            :param a_1: the first member of the progression
-            :param diff: progression difference
-            :param a_n: the last member of the progression
-            :return: f(x) = a_1 + diff * (a_n - 1)  # arithmetic progression
-            """
+                :param a_1: the first member of the progression
+                :param diff: progression difference
+                :param a_n: the last member of the progression
+                :return: f(x) = a_1 + diff * (a_n - 1)  # arithmetic progression
+                """
 
-            return a_1 + diff * (a_n - 1)
+                return a_1 + diff * (a_n - 1)
 
-        @staticmethod
-        def geometric(b_1: float, diff: float, b_n: float) -> float:
-            """
-            In mathematics, a geometric progression, also known as a geometric sequence, is a sequence of non-zero
-            numbers where each term after the first is found by multiplying the previous one by a fixed,
-            non-zero number called the common ratio. For example, the sequence 2, 6, 18, 54, ... is a geometric
-            progression with common ratio 3. Similarly 10, 5, 2.5, 1.25, ... is a geometric sequence with common
-            ratio 1/2.
-            (c) wiki
+        class Geometric:
+            @staticmethod
+            def n_num(b_1: float, diff: float, b_n: float) -> float:
+                """
+                In mathematics, a geometric progression, also known as a geometric sequence, is a sequence of non-zero
+                numbers where each term after the first is found by multiplying the previous one by a fixed,
+                non-zero number called the common ratio. For example, the sequence 2, 6, 18, 54, ... is a geometric
+                progression with common ratio 3. Similarly 10, 5, 2.5, 1.25, ... is a geometric sequence with common
+                ratio 1/2.
+                (c) wiki
 
-            :param b_1: the first member of the progression
-            :param diff: progression difference
-            :param b_n: the last member of the progression
-            :return: f(x) = b_1 * diff ** (b_n - 1)  # geometric progression
-            """
+                :param b_1: the first member of the progression
+                :param diff: progression difference
+                :param b_n: the last member of the progression
+                :return: f(x) = b_1 * diff ** (b_n - 1)  # geometric progression
+                """
 
-            return b_1 * diff ** (b_n - 1)
+                return b_1 * diff ** (b_n - 1)
 
 
 class Statistic:
