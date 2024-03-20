23a2bc25f52052dda705918912d7285db5ee5ed7
ts.kdv.raz0r <ts.kdv.raz0r@ya.ru>
2024-03-20 15:44:23 +0300

    [DOCKER] feat: Расширена структура библиотеки. Добавлены новые классы и методы.

--- docker/docker.py
+++ docker/docker.py
@@ -12,10 +12,189 @@ class Docker:
     @classmethod
     def version(
             cls,
-            options: str
+            options: str = ""
     ) -> None:
         
         os.system(command=f"docker --version {options}".strip())
+    
+    
+    class Builder:
+        
+        @classmethod
+        def build(
+                cls,
+                options: str = "",
+                path_or_url: str = ""
+        ) -> None:
+            
+            os.system(command=f"docker image build {options} {path_or_url}".strip())
+        
+        
+        @classmethod
+        def prune(cls) -> None:
+            
+            os.system(command="docker build prune")
+    
+    
+    class BuildX:
+        
+        @classmethod
+        def bake(
+                cls,
+                options: str = "",
+                target: str = ""
+        ) -> None:
+            
+            os.system(command=f"docker buildx bake {options} {target}".strip())
+        
+        @classmethod
+        def build(
+                cls,
+                options: str = "",
+                path_or_url: str = ""
+        ) -> None:
+            
+            os.system(command=f"docker buildx build {options} {path_or_url}".strip())
+        
+        @classmethod
+        def create(
+                cls,
+                options: str = "",
+                context_or_endpoint: str = ""
+        ) -> None:
+            
+            os.system(command=f"docker buildx create {options} {context_or_endpoint}".strip())
+        
+        @classmethod
+        def debug(cls) -> None:
+            
+            os.system(command="docker buildx debug")
+        
+        @classmethod
+        def debug_build(
+                cls,
+                options: str = "",
+                path_or_url: str = ""
+        ) -> None:
+            
+            os.system(command=f"docker buildx debug build {options} {path_or_url}".strip())
+        
+        @classmethod
+        def du(cls) -> None:
+            
+            os.system(command="docker buildx du")
+        
+        @classmethod
+        def imagetools(cls) -> None:
+            
+            os.system(command="docker buildx imagetools")
+        
+        @classmethod
+        def imagetools_create(
+                cls,
+                options: str = "",
+                source: str = ""
+        ) -> None:
+            
+            os.system(command=f"docker buildx imagetools create {options} {source}".strip())
+        
+        @classmethod
+        def imagetools_inspect(
+                cls,
+                options: str = "",
+                name: str = ""
+        ) -> None:
+            
+            os.system(command=f"docker buildx imagetools inspect {options} {name}".strip())
+        
+        @classmethod
+        def inspect(
+                cls,
+                name: str = ""
+        ) -> None:
+            
+            os.system(command=f"docker buildx inspect {name}".strip())
+        
+        @classmethod
+        def ls(cls) -> None:
+            
+            os.system(command="docker buildx ls")
+        
+        @classmethod
+        def prune(cls) -> None:
+            
+            os.system(command="docker buildx prune")
+        
+        @classmethod
+        def rm(
+                cls,
+                options: str = "",
+                name: str = ""
+        ) -> None:
+            
+            os.system(command=f"docker buildx rm {options} {name}".strip())
+        
+        @classmethod
+        def stop(
+            cls,
+            name: str = ""
+        ) -> None:
+            
+            os.system(command=f"docker buildx stop {name}".strip())
+        
+        @classmethod
+        def use(
+            cls,
+            options: str = "",
+            name: str = ""
+        ) -> None:
+            
+            os.system(command=f"docker buildx use {options} {name}".strip())
+        
+        @classmethod
+        def version(cls) -> None:
+            
+            os.system(command="docker buildx version")
+    
+    
+    class Checkpoint:
+        
+        @classmethod
+        def create(
+                cls,
+                options: str = "",
+                container: str = "",
+                checkpoint: str = ""
+        ) -> None:
+            
+            os.system(command=f"docker checkpoint create {options} {container} {checkpoint}".strip())
+        
+        @classmethod
+        def ls(
+                cls,
+                options: str = "",
+                container: str = "",
+        ) -> None:
+            
+            os.system(command=f"docker checkpoint ls {options} {container}".strip())
+        
+        @classmethod
+        def rm(
+                cls,
+                options: str = "",
+                container: str = "",
+                checkpoint: str = ""
+        ) -> None:
+            
+            os.system(command=f"docker checkpoint rm {options} {container} {checkpoint}".strip())
+    
+    
+    class Compose:
+        pass
+    
+    
+    class Config:
+        pass
     
     
     class Container:
@@ -27,9 +206,9 @@ class Docker:
         @classmethod
         def commit(
                 cls,
-                options: str,
                 container: str,
-                repository: str
+                repository: str,
+                options: str = ""
         ) -> None:
             
             os.system(command=f"docker container commit {options} {container} {repository}".strip())
@@ -69,7 +248,7 @@ class Docker:
         @classmethod
         def ls(
                 cls,
-                options: str
+                options: str = ""
         ) -> None:
             
             os.system(command=f"docker container list {options}".strip())
@@ -97,8 +276,8 @@ class Docker:
         @classmethod
         def rm(
                 cls,
-                options: str,
                 container: str,
+                options: str = ""
         ) -> None:
             
             os.system(command=f"docker container rm {options} {container}".strip())
@@ -106,12 +285,11 @@ class Docker:
         @classmethod
         def run(
                 cls,
-                
-                options: str,
                 image: str,
-                command: str,
-                arg: str,
-                platform: str = "linux/amd64"
+                options: str = "",
+                platform: str = "linux/amd64",
+                command: str = "",
+                arg: str = ""
         ) -> None:
             
             os.system(command=f"docker container run {options} --platform={platform} {image} {command} {arg}".strip())
@@ -119,8 +297,8 @@ class Docker:
         @classmethod
         def start(
                 cls,
-                options: str,
-                container: str
+                container: str,
+                options: str = ""
         ) -> None:
             
             os.system(command=f"docker container start {options} {container}".strip())
@@ -132,8 +310,8 @@ class Docker:
         @classmethod
         def stop(
                 cls,
-                options: str,
-                container: str
+                container: str,
+                options: str = ""
         ) -> None:
             
             os.system(command=f"docker container stop {options} {container}".strip())
@@ -153,7 +331,11 @@ class Docker:
         @classmethod
         def wait(cls) -> None:
             pass
-        
+    
+    
+    class Context:
+        pass
+    
     
     class Image:
         
@@ -180,8 +362,8 @@ class Docker:
         @classmethod
         def ls(
                 cls,
-                options: str,
-                repository: str =""
+                options: str = "",
+                repository: str = ""
         ) -> None:
             
             os.system(command=f"docker image ls {options} {repository}".strip())
@@ -193,8 +375,8 @@ class Docker:
         @classmethod
         def pull(
                 cls,
-                options: str,
-                name: str
+                name: str,
+                options: str = ""
         ) -> None:
             
             os.system(command=f"docker image pull {options} {name}".strip())
@@ -204,8 +386,13 @@ class Docker:
             pass
         
         @classmethod
-        def rm(cls) -> None:
-            pass
+        def rm(
+                cls,
+                image: str,
+                options: str = ""
+        ) -> None:
+            
+            os.system(command=f"docker image rm {options} {image}".strip())
         
         @classmethod
         def save(cls) -> None:
@@ -214,3 +401,55 @@ class Docker:
         @classmethod
         def tag(cls) -> None:
             pass
+
+
+    class Manifest:
+        pass
+    
+    
+    class Network:
+        pass
+    
+    
+    class Node:
+        pass
+    
+    
+    class Plugin:
+        pass
+    
+    
+    class Scout:
+        pass
+    
+    
+    class Secret:
+        pass
+    
+    
+    class Service:
+        pass
+    
+    
+    class Stack:
+        pass
+    
+    
+    class Swarm:
+        pass
+    
+    
+    class System:
+        pass
+    
+    
+    class Trust:
+        pass
+    
+    
+    class Volume:
+        pass
+
+
+
+
